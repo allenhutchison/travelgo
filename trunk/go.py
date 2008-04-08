e486f9dafc8654e6fedde3ff77ff8da2a39183a0
allen.hutchison <allen.hutchison@a835565c-5f4a-0410-8690-43b0940b6c7b>
2008-04-08 20:30:25 +0000

    Added a module to hold db classes, and scaffolding in main module

--- trunk/go.py
+++ trunk/go.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python
 
+import wsgiref.handlers
+
+from google.appengine.api import users
+from google.appengine.ext import webapp
+
+from models import Trip
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+		pass
+	
+
+
+def main():
+	application = webapp.WSGIApplication([('/', MainPage)],
+																				debug=True)
+	wsgiref.handlers.CGIHandler().run(application)
+
+if __name__ == "__main__":
+	main()
+
+
