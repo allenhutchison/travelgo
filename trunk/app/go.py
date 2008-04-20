63008ff5af5eee885c729ebfc3d35932d02fb6aa
allen.hutchison <allen.hutchison@a835565c-5f4a-0410-8690-43b0940b6c7b>
2008-04-20 10:54:50 +0000

    Cleaned up templates and go.py so the app now runs in the dev_appserver

--- trunk/app/go.py
+++ trunk/app/go.py
@@ -1,24 +1,39 @@
 #!/usr/bin/python
 
+import os
 import wsgiref.handlers
-
 from google.appengine.api import users
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from models import *
 
-from models import Trip
+class GoPage(webapp.RequestHandler):
+  def RenderTemplate(self, template, template_values):
+    path = os.path.join(os.path.dirname(__file__), 'templates', template)
+    self.response.out.write(webapp.template.render(path, template_values))
 
-class MainPage(webapp.RequestHandler):
+class MainPage(GoPage):
   def get(self):
-		pass
-	
+    if users.get_current_user():
+      user = users.get_current_user()
+      url = users.create_logout_url(self.request.uri)
+      url_linktext = 'Sign Out'
+    else:
+      url = users.create_login_url(self.request.uri)
+      url_linktext = 'Sign In'
+
+    template_values = {
+      "user": user,
+      "log_url": url,
+      "log_url_linktext": url_linktext
+    }
+    self.RenderTemplate('index.html', template_values)
 
 
 def main():
-	application = webapp.WSGIApplication([('/', MainPage)],
-																				debug=True)
-	wsgiref.handlers.CGIHandler().run(application)
+  application = webapp.WSGIApplication([('/', MainPage)],
+                                        debug=True)
+  wsgiref.handlers.CGIHandler().run(application)
 
 if __name__ == "__main__":
-	main()
-
-
+  main()
