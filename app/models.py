565bebdf91c0e7d6972e0ba4d97ee281bec4b921
allen.hutchison <allen.hutchison@a835565c-5f4a-0410-8690-43b0940b6c7b>
2008-04-20 10:54:50 +0000

    Cleaned up templates and go.py so the app now runs in the dev_appserver

--- app/models.py
+++ app/models.py
@@ -2,34 +2,28 @@
 
 from google.appengine.ext import db
 
+
+class Person(db.Model):
+  user = db.UserProperty()
+  phone = db.PhoneNumberProperty()
+  #Friends, other people you might take a trip with
+
+class LocationType(db.Model):
+  name = db.StringProperty(required=True)
+
+class Location(db.Model):
+  name = db.StringProperty(required=True)
+  location_type = db.ReferenceProperty(LocationType)
+  geopt = db.GeoPtProperty()
+
+class Ratings(db.Model):
+  user = db.ReferenceProperty(Person)
+  location = db.ReferenceProperty(Location)
+  rating = db.RatingProperty()  
+
 class Event(db.Model):
-	"""An event is the container that everything else lives in. It could be a concert
-	or a vacation. It could include one person or many."""
   name = db.StringProperty(required=True)
   start_date = db.DateProperty()
   end_date = db.DateProperty()
   organizer = db.ReferenceProperty(Person)
-	location = db.ReferenceProperty(Location)
-	#acl or privacy setting
-
-class Location(db.Model):
-	"""Could be any kind of location, like a hotel, a park, a city, or a country.
-	Really anything"""
-  name = db.StringProperty(required=True)
-  kind = db.ReferenceProperty(LocationType)
-	geopt = db.GeoPtProperty()
-
-class LocationType(db.Model):
-	"""Used in place of an enum in the Locaiton. This way we can add new location
-	types from the web interface later"""
-	name = db.StringProperty(required=True)
-
-class Person(db.Model):
-	user = db.UserProperty()
-	phone = db.PhoneNumberProperty()
-  #Friends, other people you might take a trip with
-
-class Ratings(db.Model):
-	user = db.ReferenceProperty(Person)
-	location = db.ReferenceProperty(Location)
-  rating = db.RatingProperty()	
+  location = db.ReferenceProperty(Location)
