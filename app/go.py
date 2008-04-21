f728d1d15de6aa70b50ea01a1d38ef64c86d27dc
allen.hutchison <allen.hutchison@a835565c-5f4a-0410-8690-43b0940b6c7b>
2008-04-21 13:06:13 +0000

    added css, refactored templates, pulled out some common functionality.

--- app/go.py
+++ app/go.py
@@ -8,27 +8,28 @@ from google.appengine.ext.webapp import template
 from models import *
 
 class GoPage(webapp.RequestHandler):
-  def RenderTemplate(self, template, template_values):
+  def RenderTemplate(self, template):
     path = os.path.join(os.path.dirname(__file__), 'templates', template)
-    self.response.out.write(webapp.template.render(path, template_values))
+    self.response.out.write(webapp.template.render(path, self.template_values))
+  
+  def CheckUser(self):
+    user = users.get_current_user()
+    if user != None:
+      url = users.create_logout_url(self.request.uri)
+      url_linktext = 'Sign out'
+    else:
+      url = users.create_login_url(self.request.uri)
+      url_linktext = 'Sign in'
+    
+    self.template_values['user'] = user
+    self.template_values['log_url'] = url
+    self.template_values['log_url_linktext'] = url_linktext
 
 class MainPage(GoPage):
   def get(self):
-    if users.get_current_user():
-      user = users.get_current_user()
-      url = users.create_logout_url(self.request.uri)
-      url_linktext = 'Sign Out'
-    else:
-      url = users.create_login_url(self.request.uri)
-      url_linktext = 'Sign In'
-
-    template_values = {
-      "user": user,
-      "log_url": url,
-      "log_url_linktext": url_linktext
-    }
-    self.RenderTemplate('index.html', template_values)
-
+    self.template_values = {}
+    self.CheckUser()
+    self.RenderTemplate('index.html')
 
 def main():
   application = webapp.WSGIApplication([('/', MainPage)],
